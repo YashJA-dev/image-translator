76a38e77bc1ba61eefe39518a11d50cb6baa626c
yash <yash13203@gmail.com>
2021-10-30 03:21:48 +0530

    camera setup

--- app/src/main/java/com/example/imagetranslater/MainActivity.java
+++ app/src/main/java/com/example/imagetranslater/MainActivity.java
@@ -1,14 +1,103 @@
 package com.example.imagetranslater;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean permissionGranted;
+    Context context;
+    Activity activity;
+    private Button captureButton;
+    private Camera camera;
+    private Preview preview;
+    private PreviewView previewView;
+    private ImageCapture imageCapture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        context=this;
+        activity=this;
+        setContentView(R.layout.camera_main);
+        captureButton=activity.findViewById(R.id.capture_img_btn);
+        permissionGranted=permissionsGranted();
+        if(permissionGranted==false){
+            ActivityCompat.requestPermissions(activity, new String[] {Manifest.permission.CAMERA}, 0);
+        }else{
+            startCamera();
+            previewView=activity.findViewById(R.id.camera_view);
+            captureButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        }
+
+
+    }
+    public boolean permissionsGranted(){
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(!permissionGranted){
+            makeToast(context,"Pls give the required permissions other wise you will not be able to use the features associated with them");
+        }else{
+            startCamera();
+        }
+    }
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.getCameraInfo()));
+    }
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> futureCameraProvider = ProcessCameraProvider.getInstance(context);
+        futureCameraProvider.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider=futureCameraProvider.get();
+                    bindPreview(cameraProvider);
+
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },ContextCompat.getMainExecutor(context));
+    }
+
+    public void makeToast(Context context,String message){
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
     }
 }
